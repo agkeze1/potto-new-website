858011b9101f19ce40d1b34eef37a6339fc7b959
agkeze1 <engr.ejikeeze@gmail.com>
2023-11-13 20:32:06 +0100

    Implemented Benefit Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import TopNav from "./components/TopNav"
-import Banner from './components/Banner'
+import Image from "next/image";
+import TopNav from "./components/core/TopNav";
+import Banner from "./components/sections/Banner";
+import Benefits from "./components/sections/Benefits";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center">
-    <TopNav/>
-    <Banner/>
+      <TopNav />
+      <Banner />
+      <Benefits />
     </main>
-  )
+  );
 }
-  
